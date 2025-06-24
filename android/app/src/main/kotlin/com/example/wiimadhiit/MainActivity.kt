e63830591daf41eaffafcf4896df28f899a257c6
WiiMadHIIT <support@wiimadhiit.com>
2025-06-24 23:41:22 +0800

    added: flutter project

---
+++ android/app/src/main/kotlin/com/example/wiimadhiit/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.wiimadhiit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ flutter_jdk17.bat
@@ -0,0 +1,4 @@
+@echo off
+set JAVA_HOME=D:\Program Files\Java\jdk-17.0.12
+set PATH=%JAVA_HOME%\bin;%PATH%
+flutter %*
